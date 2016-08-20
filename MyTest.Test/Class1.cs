1b0e05aa8ba4f4ae18b3a13e084bc4dd0a3e40fc
praveen.suluvai <suluvai@gmail.com>
2016-08-20 18:02:28 +1000

    MyTest Application initial commit

---
+++ MyTest.Test/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MyTest.Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [TestCase]
+        public void TestAddition()
+        {
+            int result = Addition.Add(1,2);
+            Assert.AreEqual(result, 3);            
+        }
+
+    }
+}
---
+++ MyTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTest
+{
+    public class Addition
+    {
+        static int Main(string[] args)
+        {
+
+            return 0;
+        }
+        public static int Add(int input1, int input2)
+        {
+            return input1 + input2;
+        }
+    }
+}
